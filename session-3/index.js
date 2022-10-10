e4cde976c7a7e285f4925b5e0379338ce3010808
Salma Ali <salma.abdifatah30@gmail.com>
2022-10-10 21:05:16 +0100

    class work

--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,26 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+ let a = 5;
+ const b = 10;
+ let c = a + b;
+
+ console.log(c)
+
+ function sayHey() {
+    console.log('Hey!');
+ };
+
+ function conversation() {
+    sayHey();
+    console.log("How are you?")
+    console.log("Goodbye")
+ };
+
+ conversation();
+
+ function futureAge(name, age) {
+    const future = age + 5
+    return "Hi " + name + " You will be " + future + " in 5 years."
+ }
+
+ console.log(futureAge("salma", 25))
