fa52cd65572de3e9ac97dd75c012fd9c909481ce
Salma Ali <salma.abdifatah30@gmail.com>
2022-10-12 15:35:43 +0100

    session 2 classwork

--- session-2/index.js
+++ session-2/index.js
@@ -1,3 +1,5 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
-console.log()
+//console.log(document.getElementById('description').innerHTLM);
+console.log(document.querySelector('#description').innerHTML);
+console.log(document.querySelector('.about').innerHTML);
