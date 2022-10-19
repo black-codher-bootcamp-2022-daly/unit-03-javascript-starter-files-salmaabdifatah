57c3cccf9587b0e9b5b8522ce061c45ec64ddba0
Salma Ali <salma.abdifatah30@gmail.com>
2022-10-19 19:42:17 +0100

    session 6

--- session-6/index.js
+++ session-6/index.js
@@ -3,11 +3,21 @@ const randomNumber = Math.round(Math.random() * 10);
 
 // ****************************************************TASK 1****************************************************
 
+// let count = 0;
+
+// while (count < 10) {
+//     console.log(randomNumber)
+//     count += 1
+// }
 
 
 
 
 
+for (let counter = 1; counter <= 10; counter = counter +1) {
+    
+    console.log(randomNumber);
+}
 
 
 
