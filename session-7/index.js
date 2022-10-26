778c5220718eb7febbffbd00380266c71920f88a
Salma Ali <salma.abdifatah30@gmail.com>
2022-10-26 21:03:30 +0100

    session 7

--- session-7/index.js
+++ session-7/index.js
@@ -29,14 +29,26 @@
 // ****************************************************TASK 3****************************************************
 
 // Notice the backticks enclosing t'challa. Read https://www.javascripttutorial.net/es6/javascript-template-literals/ for an explanation on template literals
-const people = [
-  { name: `t'challa`, age: 37 },
-  { name: 'shuri', age: 16 },
-  { name: 'killmonger', age: 32 },
-];
+// const people = [
+//   { name: `t'challa`, age: 37 },
+//   { name: 'shuri', age: 16 },
+//   { name: 'killmonger', age: 32 },
+// ];
 
-// This is a helper function that should be used in the final task of the session.
-// You should pass this function to the sort method to sort the ages of each individiual in the people array of objects
-function compare(a, b) {
-  return a.age - b.age;
-}
+// // This is a helper function that should be used in the final task of the session.
+// // You should pass this function to the sort method to sort the ages of each individiual in the people array of objects
+// function compare(a, b) {
+//   return a.age - b.age;
+// }
+
+const names = [ 'salma', 'seham', 'rukiya', 'savannah']
+
+names.sort().reverse()
+console.log(names)
+
+let numbers = [5, 325, 67, 100000, 150]
+
+numbers.sort()
+console.log(numbers)
+
+
